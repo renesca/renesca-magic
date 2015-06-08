bdf70463b00c93c6ff5b4ea8b5b3d0473715b761
Felix Dietze <felix@beerleader.net>
2015-06-09 01:57:30 +0200

    more tests for relation trait factory

--- test/src/test/scala/GenerationSpec.scala
+++ test/src/test/scala/GenerationSpec.scala
@@ -80,6 +80,7 @@ class GenerationSpec extends Specification with CodeComparison {
         q"""object T extends RootNodeTraitFactory[T]"""
       )
     }
+    //TODO: no own factory, when there is no node extending the trait
     "with local-interface" >> {
       generatedContainsCode(
         q"object A {@Node trait T {val p:String}}",
@@ -101,6 +102,27 @@ class GenerationSpec extends Specification with CodeComparison {
         q"""trait TFactory[START <: Node, +RELATION <: AbstractRelation[START, END], END <: Node]
                 extends AbstractRelationFactory[START, RELATION, END] {
               def localT(startNode: START, endNode: END): RELATION
+            }"""
+      )
+    }
+    "with local interface" >> {
+      generatedContainsCode(
+        q"object A {@Relation trait T {val p:String}}",
+        q"""trait TFactory[START <: Node, +RELATION <: AbstractRelation[START, END], END <: Node]
+                extends AbstractRelationFactory[START, RELATION, END] {
+              def localT(startNode: START, endNode: END, p: String): RELATION
+            };"""
+      )
+    }
+
+    //TODO: "with own factory" >> { }
+
+    "with superType factories" >> {
+      generatedContainsCode(
+        q"object A {@Relation trait T; @Relation trait X extends T}",
+        q"""trait XFactory[START <: Node, +RELATION <: AbstractRelation[START, END], END <: Node]
+                extends AbstractRelationFactory[START, RELATION, END] with TFactory[START, RELATION, END] {
+              def localX(startNode: START, endNode: END): RELATION
             }"""
       )
     }
