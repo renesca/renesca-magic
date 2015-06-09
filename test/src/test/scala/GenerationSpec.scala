6d898f631209c4b56ebc87c59feb57f5af8a94de
Felix Dietze <felix@beerleader.net>
2015-06-09 02:30:57 +0200

    test node with inherited properties

--- test/src/test/scala/GenerationSpec.scala
+++ test/src/test/scala/GenerationSpec.scala
@@ -144,7 +144,7 @@ class GenerationSpec extends Specification with CodeComparison {
       )
     }
     "with super factory" >> {
-      generatedContainsCodePrint(
+      generatedContainsCode(
         q"object A {@Node trait T; @Node class N extends T}",
         q"""object N extends TFactory[N] {
               def wrap(node: raw.Node) = new N(node);
@@ -155,6 +155,54 @@ class GenerationSpec extends Specification with CodeComparison {
               };
               def localT(): N = local()
             } """
+      )
+    }
+    "with properties" >> {
+      generatedContainsCode(
+        q"object A {@Node class N {val p:String; var x:Int}}",
+        q"""object N extends NodeFactory[N] {
+              def wrap(node: raw.Node) = new N(node);
+              val label = raw.Label("N");
+              def local(p: String, x: Int): N = {
+                val node = wrap(raw.Node.local(List(label)));
+                node.node.properties.update("p", p);
+                node.node.properties.update("x", x);
+                node
+              }
+            }"""
+      )
+    }
+    "with inherited properties" >> {
+      generatedContainsCode(
+        q"object A {@Node trait T {val p:String; var x:Int}; @Node class N extends T}",
+        q"""object N extends TFactory[N] {
+              def wrap(node: raw.Node) = new N(node);
+              val label = raw.Label("N");
+              def local(p: String, x: Int): N = {
+                val node = wrap(raw.Node.local(List(label)));
+                node.node.properties.update("p", p);
+                node.node.properties.update("x", x);
+                node
+              };
+              def localT(p: String, x: Int): N = local(p, x)
+            }; """
+      )
+    }
+    "with twice inherited properties" >> {
+      generatedContainsCode(
+        q"object A {@Node trait T {val p:String; var x:Int}; @Node trait X extends T; @Node class N extends X}",
+        q"""object N extends XFactory[N] {
+              def wrap(node: raw.Node) = new N(node);
+              val label = raw.Label("N");
+              def local(p: String, x: Int): N = {
+                val node = wrap(raw.Node.local(List(label)));
+                node.node.properties.update("p", p);
+                node.node.properties.update("x", x);
+                node
+              };
+              def localX(p: String, x: Int): N = local(p, x)
+            };"""
+            //TOOD: concrete implementation of def localT?
       )
     }
   }
