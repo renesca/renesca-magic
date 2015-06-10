e0e05954c6b7bf79358f66451ee45b919770993d
Felix Dietze <felix@beerleader.net>
2015-06-10 16:28:09 +0200

    simplify node factory spec

--- test/src/test/scala/codegeneration/NodeFactorySpec.scala
+++ test/src/test/scala/codegeneration/NodeFactorySpec.scala
@@ -23,63 +23,45 @@ class NodeFactorySpec extends Specification with CodeComparison {
   "with super factory" >> {
     generatedContainsCode(
       q"object A {@Node trait T; @Node class N extends T}",
-      q"""object N extends TFactory[N] {
-              def wrap(node: raw.Node) = new N(node);
-              val label = raw.Label("N");
-              def local(): N = {
-                val node = wrap(raw.Node.local(List(label)));
-                node
-              };
-              def localT(): N = local()
-            } """
+      q"""object N extends TFactory[N] """,
+      q"""def localT(): N = local()"""
     )
   }
   "with properties" >> {
     generatedContainsCode(
       q"object A {@Node class N {val p:String; var x:Int}}",
-      q"""object N extends NodeFactory[N] {
-              def wrap(node: raw.Node) = new N(node);
-              val label = raw.Label("N");
-              def local(p: String, x: Int): N = {
-                val node = wrap(raw.Node.local(List(label)));
-                node.node.properties.update("p", p);
-                node.node.properties.update("x", x);
-                node
-              }
-            }"""
+      q"""def local(p: String, x: Int): N = {
+            val node = wrap(raw.Node.local(List(label)));
+            node.node.properties.update("p", p);
+            node.node.properties.update("x", x);
+            node
+          } """
     )
   }
   "with inherited properties" >> {
     generatedContainsCode(
       q"object A {@Node trait T {val p:String; var x:Int}; @Node class N extends T}",
-      q"""object N extends TFactory[N] {
-              def wrap(node: raw.Node) = new N(node);
-              val label = raw.Label("N");
-              def local(p: String, x: Int): N = {
-                val node = wrap(raw.Node.local(List(label)));
-                node.node.properties.update("p", p);
-                node.node.properties.update("x", x);
-                node
-              };
-              def localT(p: String, x: Int): N = local(p, x)
-            }; """
+      q"""def local(p: String, x: Int): N = {
+            val node = wrap(raw.Node.local(List(label)));
+            node.node.properties.update("p", p);
+            node.node.properties.update("x", x);
+            node
+          }""",
+      q""" def localT(p: String, x: Int): N = local(p, x) """
     )
   }
+  //TODO: different local/localT
   "with indirectly inherited properties" >> {
     generatedContainsCode(
       q"object A {@Node trait T {val p:String; var x:Int}; @Node trait X extends T; @Node class N extends X}",
-      q"""object N extends XFactory[N] {
-              def wrap(node: raw.Node) = new N(node);
-              val label = raw.Label("N");
-              def local(p: String, x: Int): N = {
-                val node = wrap(raw.Node.local(List(label)));
-                node.node.properties.update("p", p);
-                node.node.properties.update("x", x);
-                node
-              };
-              def localX(p: String, x: Int): N = local(p, x)
-            };"""
-      //TOOD: concrete implementation of def localT?
+      q"""def local(p: String, x: Int): N = {
+            val node = wrap(raw.Node.local(List(label)));
+            node.node.properties.update("p", p);
+            node.node.properties.update("x", x);
+            node
+          }""",
+      q""" def localX(p: String, x: Int): N = local(p, x) """
+      //TOOD: why no localT?
     )
   }
 }
--- test/src/test/scala/codegeneration/SchemaSpec.scala
+++ test/src/test/scala/codegeneration/SchemaSpec.scala
@@ -25,6 +25,8 @@ class SchemaSpec extends Specification with CodeComparison {
          }
        }
       } """)
+
+    //TODO: custom code
   }
 
 }
